40f6c14a013a1f1c7a4d2952d0961b0aac242fe9
castlho <hocastiel@gmail.com>
2020-10-29 00:11:35 +0800

    ADD layout component

---
+++ src/routes/_layout.svelte
@@ -0,0 +1,43 @@
+<script>
+	import Footer from '../components/Footer.svelte'
+	import Nav from '../components/Nav.svelte'
+
+	export let segment
+</script>
+
+<style>
+	.app {
+		width: 100%;
+		margin-left: auto;
+		margin-right: auto;
+	}
+
+	@media (max-width: 767px) {
+		.app {
+			max-width: 500px;
+		}
+		.app :global(.ftr) {
+			margin: 3.5rem .5rem 2rem;
+		}
+	}
+
+	@media (min-width: 768px) {
+		.app {
+			max-width: 704px;
+		}
+		.app :global(.ftr) {
+			margin-top: 5rem;
+			margin-bottom: 3rem;
+		}
+	}
+</style>
+
+<div class="app">
+	{#if !!segment}
+		<Nav {segment} />
+	{/if}
+	<main>
+		<slot />
+		<Footer />
+	</main>
+</div>
