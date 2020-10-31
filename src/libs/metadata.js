dbcd66e37ace6536f1c1a9e8bd2ee653c3fc43bd
castlho <hocastiel@gmail.com>
2020-10-31 19:38:13 +0800

    UPDATE meta description

--- src/libs/metadata.js
+++ src/libs/metadata.js
@@ -1,5 +1,5 @@
 /** Site description */
-export const siteDescription = '每次聽音樂，都是用聽力換經歷。十年過去，開始聽不清低音，卻得到回憶滿滿。'
+export const siteDescription = '每次聽音樂，都是用聽力換經歷。十年過去，開始聽不清低頻聲音，卻得到回憶滿滿。'
 
 /** Site title */
 export const siteTitle = 'Permanent Hearing Damage'
